1eda50c7bf041e593686510c2932e30bb8dff84c
dzou <dzou@company.com>
2021-03-22 10:58:22 +0100

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom'
+
+
+
+import Home from './pages/home/index.js'
+
+import Navbar from './components/navbar/index.js'
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/" component={Home} />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
